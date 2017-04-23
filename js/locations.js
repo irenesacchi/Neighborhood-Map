785733361769ed7f5363373a1aa35cd0be473ab0
Irene Sacchi <irene.lueckmann@gmail.com>
2017-04-23 17:33:19 +0200

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -2,26 +2,32 @@
 //list of Places
 var Places = [{
     name: 'Mühlenstraße',
+    city: 'Münster',
     location: {lat: 51.959286, lng: 7.620825},
     img : '<img src="img/Mühlenstraße.jpg">'
 }, {
     name: 'Am Kanonengraben',
+    city: 'Münster',
     location: {lat: 51.956419, lng: 7.620826},
     img : '<img src="img/Kanonengraben.jpg">'
 }, {
     name: 'Windthorststraße',
+    city: 'Münster',
     location: {lat: 51.957431, lng: 7.630752},
     img : '<img src="img/Windthorststraße.jpg">'
 }, {
     name: 'Servatiiplatz',
+    city: 'Münster',
     location: {lat: 51.9600872, lng: 7.6322295},
     img : '<img src="img/Servatiiplatz.jpg">'
 }, {
   	name: 'Coerdeplatz',
+  	city: 'Münster',
   	location: {lat: 51.967558, lng: 7.628810},
   	img : '<img src="img/Coerdeplatz.jpg">',
 }, {
   	name: 'Kleimannstraße',
+  	city: 'Münster',
   	location: {lat: 51.968184, lng: 7.622125},
   	img : '<img src="img/Kleimannstraße.jpg">',
 }];
