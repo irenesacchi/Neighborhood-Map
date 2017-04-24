a3e6153c5d7b0445426b61ea8f77b27da7599e4c
Irene Sacchi <irene.lueckmann@gmail.com>
2017-04-24 17:55:35 +0200

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -2,33 +2,32 @@
 //list of Places
 var Places = [{
     name: 'Mühlenstraße',
-    city: 'Münster',
+    block: 'Münster-Mitte',
     location: {lat: 51.959286, lng: 7.620825},
     img : '<img src="img/Muehlenstraße.jpg">'
 }, {
     name: 'Am Kanonengraben',
-    city: 'Münster',
+    block: 'Münster-Aaseestadt',
     location: {lat: 51.956419, lng: 7.620826},
     img : '<img src="img/Kanonengraben.jpg">'
 }, {
     name: 'Windthorststraße',
-    city: 'Münster',
+    block: 'Münster-Südviertel',
     location: {lat: 51.957431, lng: 7.630752},
     img : '<img src="img/Windthorststraße.jpg">'
 }, {
     name: 'Servatiiplatz',
-    city: 'Münster',
+    block: 'Münster-Hansaviertel',
     location: {lat: 51.9600872, lng: 7.6322295},
     img : '<img src="img/Servatiiplatz.jpg">'
 }, {
   	name: 'Coerdeplatz',
-  	city: 'Münster',
+  	block: 'Münster-Mauritzviertel',
   	location: {lat: 51.967558, lng: 7.628810},
   	img : '<img src="img/Coerdeplatz.jpg">',
 }, {
   	name: 'Kleimannstraße',
-  	city: 'Münster',
+  	block: 'Münster-Kreuzviertel',
   	location: {lat: 51.968184, lng: 7.622125},
   	img : '<img src="img/Kleimannstraße.jpg">',
 }];
-
